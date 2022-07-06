b32775fff3a1643aa245321f615808c6af727cc6
pcar22 <pcarranza2@comcast.net>
2022-07-06 11:25:39 -0600

    Corrected indentation, code still needs work

--- Resources/PyPoll.py
+++ Resources/PyPoll.py
@@ -28,7 +28,8 @@ with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
     # Read the header row.
     headers = next(file_reader)
-    
+    # print(headers) Just to test the 2 code lines above. It works, or at least it printed the headers.
+     
     # Print each row in the CSV file.
     for row in file_reader:
         # 2. Add to the total vote count.
@@ -52,45 +53,35 @@ with open(file_to_save, "w") as txt_file:
         f"Total Votes: {total_votes:,}\n"
         f"-------------------------\n")
     print(election_results, end="")
-
     txt_file.write(election_results)
-
-
     for candidate_name in candidate_votes:
         # Retrieve vote count and percentage.
         votes = candidate_votes[candidate_name]
         vote_percentage = float(votes) / float(total_votes) * 100
         candidate_results = (
             f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
-
         print(candidate_results)
-
-        # Save the candidate results to our text file.
+       # Save the candidate results to our text file.
         txt_file.write(candidate_results)
-
-        # Print each candidate, their voter count, and percentage to the terminal.
-        # print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n") # Commented out this line to write the results
-            # Determine winning vote count, winning percentage, and candidiate. Module 3.5.5 if statement inside the for loop.
-        if (votes > winning_count) and (vote_percentage > winning_percentage): # Determines if the votes are greater than the winning count. 
+       # Print each candidate, their voter count, and percentage to the terminal.
+       # print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n") # Commented out this line to write the results
+           # Determine winning vote count, winning percentage, and candidiate. Module 3.5.5 if statement inside the for loop.
+        if (votes > winning_count) and (vote_percentage > winning_percentage): # Determines if the votes are greater than the winning count.
             winning_count = votes
             winning_candidate = candidate_name
             winning_percentage = vote_percentage
-        
-    # To do: print out each candidate's name, vote count, and percentage of
-    # votes to the terminal.
-        # print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
-
-    # Print the winning candidates results to the terminal.
+   # To do: print out each candidate's name, vote count, and percentage of
+   # votes to the terminal.
+       # print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+   # Print the winning candidates results to the terminal.
     winning_candidate_summary = (
         f"--------------------\n"
         f"Winner: {winning_candidate}\n"
         f"Winning Vote Count: {winning_count:,}\n"
         f"Winning Percentage: {winning_percentage:.1f}%\n"
         f"-------------------------\n")
-
     print(winning_candidate_summary) # Commented line to write to text file.
-
-# Save the winning candidate's results to the text file. 
-txt_file.write(winning_candidate_summary)
+    # Save the winning candidate's results to the text file.
+    txt_file.write(winning_candidate_summary)
 
                                                
